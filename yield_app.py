eaaa0dc66d2cb0ced55d57a388ca82e6488ab303
Aisha Abdulkarim <105824064+aisha738@users.noreply.github.com>
2025-03-07 23:37:06 +0100

    Update yield_app.py

--- yield_app.py
+++ yield_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
 # Page Config (must be the first Streamlit command)
 st.set_page_config(page_title="Crop Yield Prediction", layout="centered")
